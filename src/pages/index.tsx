5efb7b1dc8a1922f7e74fcaf8823f565fb4d3b8c
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-11-27 21:23:37 +0100

    added search, added few pages, added Bash, JSON in config and changed dracula to palenight theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docusaurus Documentation - 5min ⏱️
           </Link>
         </div>
       </div>
